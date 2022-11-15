cc8de62def00eec250c4952ef8fc44c14fa494e9
chunuz <pnjukia@gmail.com>
2022-11-15 07:57:06 +0300

    app.js updates

--- app.js
+++ app.js
@@ -21,17 +21,17 @@ $(function() {
 // function
 
 //  function to toggle what we do
-// function toggleWhatWeDo() {
-//     $(".service").on("click", function() {
-//         if ($(".service-details", this).is(":visible")) {
-//             $(".service-details", this).hide();
-//             $(".service-card", this).fadeIn("slow");
-//         } else {
-//             $(".service-details", this).fadeIn("slow");
-//             $(".service-card", this).hide();
-//         }
-//     });
-// }
+function toggleWhatWeDo() {
+    $(".service").on("click", function() {
+        if ($(".service-details", this).is(":visible")) {
+            $(".service-details", this).hide();
+            $(".service-card", this).fadeIn("slow");
+        } else {
+            $(".service-details", this).fadeIn("slow");
+            $(".service-card", this).hide();
+        }
+    });
+}
 
 // validate contact form
 
