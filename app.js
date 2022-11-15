1c45940fae5008fad0982a36b01742e12bca2f8c
chunuz <pnjukia@gmail.com>
2022-11-15 08:17:26 +0300

    app.js updates

--- app.js
+++ app.js
@@ -61,7 +61,7 @@ function removeFormErrors() {
 function clearForm() {
     contactForm.find("input,textarea").each(function() {
         $(this).val("");
-//     });
+    });
 // }
 
 // removeFormErrors();
